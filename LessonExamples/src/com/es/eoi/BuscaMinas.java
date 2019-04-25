c7bbdb6371264d295338224efa8caf72898c681d
jjimerod <jjimerod@everis.com>
2019-04-25 18:19:13 +0200

    commit

--- LessonExamples/src/com/es/eoi/BuscaMinas.java
+++ LessonExamples/src/com/es/eoi/BuscaMinas.java
@@ -4,31 +4,36 @@ public class BuscaMinas {
 	
 	public static void main(String [] args) {
 		
-		int altura=10;
-		int anchura=10;
+		int[][] tablero = createTablet(10,10);
 		
-		int[][] tablero=new int[altura][anchura];	
+		String tmp="";
+		
+		paintTablet(tablero, tmp);
+				
+		
+	}
+
+	private static int[][] createTablet(int height,int width) {
+						
+		int[][] tablero=new int[height][width];	
 		
 		tablero[2][8]=1;
 		tablero[1][6]=1;
 		tablero[5][2]=1;
 		
-		String tmp="";
+		return tablero;
+	}
+
+	private static void paintTablet(int[][] tablero, String tmp) {
 		
-		for (int i = 0; i < tablero.length; i++) {			
-			
-			int[] js = tablero[i];
-			
+		for (int i = 0; i < tablero.length; i++) {	
+			int[] js = tablero[i];			
 			for (int j = 0; j < js.length; j++) {
 				tmp = tmp + js[j] + "|";
 			}
 			tmp= tmp+"\n";							
-		}
-		
-		
+		}	
 		System.out.println(tmp);
-				
-		
 	}
 
 }
