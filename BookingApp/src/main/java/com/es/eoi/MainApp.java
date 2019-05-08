692a44bd23e8b315ba73cbef0dd0dfe123b6d2e8
jj1302 <josejuan.jimenez83@gmail.com>
2019-05-08 18:44:37 +0200

    commit

--- BookingApp/src/main/java/com/es/eoi/MainApp.java
+++ BookingApp/src/main/java/com/es/eoi/MainApp.java
@@ -1,11 +1,18 @@
 package com.es.eoi;
 
+import java.util.List;
+
 import com.es.eoi.entity.Booking;
+import com.es.eoi.entity.Client;
+import com.es.eoi.entity.Trip;
 import com.es.eoi.service.MenuService;
 
-public class MainApp {
+public class MainApp {	
+	
+	public static List<Booking> bookings;
+	public static List<Client> clients;
+	public static List<Trip> trips;	
 
-	static Booking[] bookings = new Booking[50];
 
 	public static void main(String[] args) {
 
--- BookingApp/src/main/java/com/es/eoi/service/ClientService.java
+++ BookingApp/src/main/java/com/es/eoi/service/ClientService.java
@@ -1,11 +1,20 @@
 package com.es.eoi.service;
 
+import com.es.eoi.MainApp;
+import com.es.eoi.entity.Client;
+
 public class ClientService {
 	
 	
 	public static void findClients() {
 		
 		//TODO logica de encontrar clientes
+	}
+	
+	public static void addClient(Client client) {
+		
+		MainApp.clients.add(client);
+		
 	}
 
 }
