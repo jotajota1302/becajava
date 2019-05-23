c1a1a41ad13fbe64450480f8c6a2e92395948689
jjimerod <jjimerod@everis.com>
2019-05-23 17:15:32 +0200

    refactor

--- facenet/src/main/java/es/eoi/facenet/rest/EntityRestController.java
+++ facenet/src/main/java/es/eoi/facenet/rest/EntityRestController.java
@@ -1,12 +1,12 @@
-package es.eoi.rest;
+package es.eoi.facenet.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import es.eoi.redsocial.entity.TestEntity;
-import es.eoi.services.EntityService;
+import es.eoi.facenet.entities.TestEntity;
+import es.eoi.facenet.services.EntityService;
  
 @RestController
 public class EntityRestController {
