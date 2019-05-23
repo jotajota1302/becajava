dbb5320c1ce455cdcab7e2cb0fb9a9fae930b54e
Angel Banegas <mcinegas@gmail.com>
2019-05-23 18:41:38 +0200

    User acabado, solo falta mapear AB - 18:41

--- facenet/src/main/java/es/eoi/facenet/entities/User.java
+++ facenet/src/main/java/es/eoi/facenet/entities/User.java
@@ -1,9 +1,84 @@
 package es.eoi.facenet.entities;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
 
 @Entity
 public class User {
 
+	private int id;
 	
+	private String name;
+	
+	private String surName;
+	
+	private Date birthDate;
+	
+	private Date startDate;
+	
+	private String userName;
+	
+	public User() {
+		
+	}
+
+	public User(int id, String name, String surName, Date birthDate, Date startDate, String userName) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surName = surName;
+		this.birthDate = birthDate;
+		this.startDate = startDate;
+		this.userName = userName;
+	}
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 }
