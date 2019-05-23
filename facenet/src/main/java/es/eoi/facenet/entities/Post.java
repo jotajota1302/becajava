3275539e2924aba50bf815c83eefff49e76ea7a3
JuanCarlosCB <jccb_11@hotmail.com>
2019-05-23 19:22:11 +0200

    Create entity post, create entity relationship

---
+++ facenet/src/main/java/es/eoi/facenet/entities/Post.java
@@ -0,0 +1,99 @@
+package es.eoi.facenet.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "POSTS")
+public class Post {
+
+	@Id
+	@Column(name="ID", unique=true)
+	private int id;
+	
+	@Column(name="CONTENT")
+	private String content;
+	
+	@Column(name="PUBLISHDATE")
+	private Date publishdate;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy="post")
+	private List<Reaction> reactions;
+	//falta crear entidad Reaction
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_user")
+	private User user;
+	
+	public Post() {
+	}
+
+	public Post(int id, String content, Date publishdate, List<Reaction> reactions, User user) {
+		this.id = id;
+		this.content = content;
+		this.publishdate = publishdate;
+		this.reactions = reactions;
+		this.user = user;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+	public Date getPublishdate() {
+		return publishdate;
+	}
+
+
+	public void setPublishdate(Date publishdate) {
+		this.publishdate = publishdate;
+	}
+
+
+	public List<Reaction> getReactions() {
+		return reactions;
+	}
+
+
+	public void setReactions(List<Reaction> reactions) {
+		this.reactions = reactions;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+}
