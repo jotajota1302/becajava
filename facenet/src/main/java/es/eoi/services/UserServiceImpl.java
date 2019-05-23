d0ce38b40ba00c0d7e49bc03a40922d6798da4f6
Mario Martínez <martinezcanovasmario@gmail.com>
2019-05-23 19:14:19 +0200

    Changes in UserService

--- facenet/src/main/java/es/eoi/services/UserServiceImpl.java
+++ facenet/src/main/java/es/eoi/services/UserServiceImpl.java
@@ -1,8 +1,47 @@
 package es.eoi.services;
 
+import java.util.Date;
 import java.util.List;
 
+import es.eoi.redsocial.entity.User;
+
 public class UserServiceImpl implements UserService {
+
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findByIdFullInfo(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createUser(String name, String surname, Date birthdate, String user, String pass) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateSurnameById(int id, String surname) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User login(String username, String pass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 ;
