80f9465e1b75a7266828b85f327fd729316e18c6
jotazip <josegnz93@gmail.com>
2019-05-23 19:39:03 +0200

    update of event, assist

--- facenet/src/main/java/es/eoi/facenet/entities/Assist.java
+++ facenet/src/main/java/es/eoi/facenet/entities/Assist.java
@@ -1,22 +1,5 @@
 package es.eoi.facenet.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "Assistents")
-public class Assist {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private String state;
-	
-}
-package es.eoi.facenet.entities;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
