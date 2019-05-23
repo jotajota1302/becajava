01e3d6e2dfc69efa172694b81aaa9529b882d697
jjimerod <jjimerod@everis.com>
2019-05-23 16:30:55 +0200

    commit

--- facenet/src/main/java/es/eoi/facenet/FacenetApplication.java
+++ facenet/src/main/java/es/eoi/facenet/FacenetApplication.java
@@ -1,6 +1,7 @@
 package es.eoi.facenet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "es.eoi.repositories")
 @EntityScan( basePackages = {"es.eoi"} )
 @ComponentScan(basePackages = "es.eoi")
+@EnableAutoConfiguration
 public class FacenetApplication {
 
 	public static void main(String[] args) {
