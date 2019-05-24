78b04443589ea2f8cbeb5d943feecb7b9cf334cb
Tomás <Toms@Toms>
2019-05-24 20:17:10 +0200

    Commit

--- facenet/src/main/java/es/eoi/redsocial/repository/UserRepository.java
+++ facenet/src/main/java/es/eoi/redsocial/repository/UserRepository.java
@@ -1,8 +1,24 @@
 package es.eoi.redsocial.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import es.eoi.redsocial.entity.User;
 
-public interface UserRepository   extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	@Query("select * from user * where u.id = ?")
+	User findById(int id);
+
+	@Query("select * from user * where user like ? and pass like ?")
+	User login(String user, String pass);
+	
+	@Query("select * from relationship * where relationship like friend")
+	User friendRelationships(String friendlist);
+	
+	@Query("select * from relationship * where relationship like pending")
+	User pendingRelationships(String pendinglist);
 	
 }
