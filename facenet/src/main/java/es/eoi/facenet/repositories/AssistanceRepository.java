ce0a99ed9ea8b6e1253a3067479cfd75b670092e
admin <admin@ALC-9MS36S1.usersad.everis.int>
2019-05-24 17:47:07 +0200

    message Service

--- facenet/src/main/java/es/eoi/facenet/repositories/AssistanceRepository.java
+++ facenet/src/main/java/es/eoi/facenet/repositories/AssistanceRepository.java
@@ -1,9 +1,17 @@
 package es.eoi.facenet.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import es.eoi.facenet.entities.Assistance;
+import es.eoi.facenet.entities.User;
+
 
 public interface AssistanceRepository extends JpaRepository<Assistance, Integer> { 
+	@Query("Select u from user u where id in (Select a.user.id_user from Assistance a where a.state='yes')")
+	List<User> findByYesAssistance();
 
 }
--- facenet/src/main/java/es/eoi/facenet/services/MessageService.java
+++ facenet/src/main/java/es/eoi/facenet/services/MessageService.java
@@ -1,5 +1,16 @@
 package es.eoi.facenet.services;
 
+import java.util.Date;
+import java.util.List;
+
+import es.eoi.facenet.entities.Message;
+import es.eoi.facenet.entities.User;
+
 public interface MessageService  {
+	
+	public Message findById(int id);
+	public boolean createMessage(String content, Date publishdate, User user);
+	public boolean deleteMessage(int id);
+	public List<Message> findByUserId(int id);
 
 }
