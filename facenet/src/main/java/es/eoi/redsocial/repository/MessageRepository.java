3710d2849c36d4ca49c860de5a650af661c29738
Brayan Florez <bflorez@outlook.com>
2019-05-24 16:19:23 +0200

    my commit

--- facenet/src/main/java/es/eoi/redsocial/repository/MessageRepository.java
+++ facenet/src/main/java/es/eoi/redsocial/repository/MessageRepository.java
@@ -1,8 +1,5 @@
 package es.eoi.redsocial.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import es.eoi.redsocial.entity.Message;
-
-public interface MessageRepository extends JpaRepository<Message, Integer>{
+public interface MessageRepository {
 
 }
