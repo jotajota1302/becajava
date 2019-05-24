5442f214c5d9d4163ee05af8d458edd3b4444d5d
admin <admin@ALC-1DNCLQ1.usersad.everis.int>
2019-05-24 19:46:29 +0200

    AssistanceServicemethods

--- facenet/src/main/java/es/eoi/facenet/services/AssistanceServiceImpl.java
+++ facenet/src/main/java/es/eoi/facenet/services/AssistanceServiceImpl.java
@@ -1,8 +1,13 @@
 package es.eoi.facenet.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import es.eoi.facenet.entities.Assistance;
+import es.eoi.facenet.entities.Event;
+import es.eoi.facenet.entities.User;
 import es.eoi.facenet.repositories.AssistanceRepository;
 
 @Service
@@ -10,10 +15,44 @@ public class AssistanceServiceImpl implements AssistanceService {
 
 	@Autowired
 	private AssistanceRepository repository;
+
+	@Override
+	public List<User> findByUsersYesAssistance() {
+		return repository.findByUsersYesAssistance();
+	}
+
+	@Override
+	public List<User> findByUsersNotAssistance() {
+		return repository.findByUsersNotAssistance();
+	}
+
+	@Override
+	public List<Event> findByEventYesAssistance() {
+		return repository.findByEventYesAssistance();
+	}
+
+	@Override
+	public List<Event> findByEventNotAssistance() {
+		return repository.findByEventNotAssistance();
+	}
+
+	@Override
+	public boolean notassist(Event event, User user) {
+		Assistance assistance = new Assistance("YES", event, user);
+		if(repository.saveAndFlush(assistance) != null) {
+			return true;
+		} else {
+			return false;
+			}
+	}
+
+	@Override
+	public boolean yesassist(Event event, User user) {
+		Assistance assistance = new Assistance("NO", event, user);
+		if(repository.saveAndFlush(assistance) != null) {
+			return true;
+		} else {
+			return false;
+			}
+	}
 }
-	
-
-
-
-
-
