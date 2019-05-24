fbb2f7ec212093860b26bc9b8d46f56b5627104a
Mario Martínez <martinezcanovasmario@gmail.com>
2019-05-24 18:09:04 +0200

    Update Services Mario

--- facenet/src/main/java/es/eoi/redsocial/entity/Relationship.java
+++ facenet/src/main/java/es/eoi/redsocial/entity/Relationship.java
@@ -38,4 +38,37 @@ public class Relationship {
 		this.user1 = user1;
 		this.user2 = user2;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public User getUser1() {
+		return user1;
+	}
+
+	public void setUser1(User user1) {
+		this.user1 = user1;
+	}
+
+	public User getUser2() {
+		return user2;
+	}
+
+	public void setUser2(User user2) {
+		this.user2 = user2;
+	}
+	
 }
