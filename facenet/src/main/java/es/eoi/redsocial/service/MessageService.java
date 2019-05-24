4a8e0f54479234a9e02ccf13ac0ce0a27bb094d5
Mario Martínez <martinezcanovasmario@gmail.com>
2019-05-24 16:53:47 +0200

    UpdateService Mario

--- facenet/src/main/java/es/eoi/redsocial/service/MessageService.java
+++ facenet/src/main/java/es/eoi/redsocial/service/MessageService.java
@@ -1,5 +1,17 @@
 package es.eoi.redsocial.service;
 
-public interface MessageService {
+import java.util.Date;
+import java.util.List;
 
+import es.eoi.redsocial.entity.Message;
+import es.eoi.redsocial.repository.MessageRepository;
+
+public interface MessageService {
+ 
+	public Message findById(int id);
+	public List<Message> findByUserId(int id);
+	public Message createPost(String content);
+	public void deletePost(int id);
+	
+	
 }
--- facenet/src/main/java/es/eoi/redsocial/service/MessageServiceImpl.java
+++ facenet/src/main/java/es/eoi/redsocial/service/MessageServiceImpl.java
@@ -1,5 +1,33 @@
 package es.eoi.redsocial.service;
 
+import java.util.List;
+
+import es.eoi.redsocial.entity.Message;
+
 public class MessageServiceImpl implements MessageService{
+
+	@Override
+	public Message findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> findByUserId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Message createPost(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletePost(int id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
