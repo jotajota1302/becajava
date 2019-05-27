9ebd8dfdb183d7475e43979cf0216493ff843870
Angel Banegas <mcinegas@gmail.com>
2019-05-27 16:39:15 +0200

    Metodo PUT tested

--- facenet/src/main/java/es/eoi/facenet/rest/UsersController.java
+++ facenet/src/main/java/es/eoi/facenet/rest/UsersController.java
@@ -3,11 +3,9 @@ package es.eoi.facenet.rest;
 import java.util.Date;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,19 +21,20 @@ public class UsersController {
 	@Autowired
 	UserService service;
 
-	
+	//Test okey
 	@RequestMapping(method = RequestMethod.GET, value = "/users")
 	public List<User> allusers(){
 		return service.findAll();
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/users/{id}", params = {"id"})
-	public User user(
-			@PathParam(value = "id" ) int id
-			) {
+	//Test okey
+	@RequestMapping(method = RequestMethod.GET, value = "/users/{id}")
+	public User user (@PathVariable int id ) 
+			 {
 		return service.findById(id);
 	}
 	
+	// Test okey
 	@RequestMapping(method = RequestMethod.POST, value = "/users",params = {"name", "surname", "birthdate", "username", "pass"} )
 	public User newUser(
 			@RequestParam(value = "name") String name,
@@ -45,42 +44,41 @@ public class UsersController {
 			@RequestParam(value = "pass") String pass
 			) {
 		User user = new User();
+		Date date = new Date();
 		
 		user.setName(name);
 		user.setSurName(surName);
 		user.setBirthDate(birthDate);
 		user.setUserName(userName);
 		user.setPass(pass);
+		user.setStartDate(date);
 		
 		return service.save(user);
 	}
 	
+	
 	@RequestMapping(method = RequestMethod.PUT, value = "/users/{id}", params = {"surname"})
-	public boolean updateUser(
-			@PathParam(value = "id") int id,
-			@RequestParam(value = "surname") String surName
+	public User updateUser(
+			
+			@PathVariable int id,
+			@RequestParam(value = "surname") String surname
+			
 			) {
 		
-		if(service.existsById(id) == true) {
+			User user = service.findById(id);
 			
-			User user = new User();
+			user.setSurName(surname);
 			
-			user.setSurName(surName);
-			
-			 service.save(user);
-			
-			 return true;
+			return service.save(user);
 		}
-		 return false;
-	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "/login", params = {"username", "pass" })
-	public User login(
-			@RequestParam(value = "username") String userName,
-			@RequestParam(value = "pass") String pass
-			@Query(value = "SELECT * FROM USERS WHERE USERNAME LIKE = ? AND PASS = ?")
-			
-			
-			) {
-	return null;
+	 	
+//	@RequestMapping(method = RequestMethod.POST, value = "/login", params = {"username", "pass" })
+//	public User login(
+//			@RequestParam(value = "username") String userName,
+//			@RequestParam(value = "pass") String pass
+//			@Query(value = "SELECT * FROM USERS WHERE USERNAME LIKE = ? AND PASS = ?")
+//			
+//			
+//			) {
+//	return null;
 }
