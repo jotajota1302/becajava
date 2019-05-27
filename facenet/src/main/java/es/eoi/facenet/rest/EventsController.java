01af5fff9ef02a641fae0325b405f62c615d611f
admin <admin@ALC-1DNCLQ1.usersad.everis.int>
2019-05-27 16:21:19 +0200

    EventControllers

--- facenet/src/main/java/es/eoi/facenet/rest/EventsController.java
+++ facenet/src/main/java/es/eoi/facenet/rest/EventsController.java
@@ -11,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.eoi.facenet.dto.AssistanceDto;
 import es.eoi.facenet.dto.EventDto;
+import es.eoi.facenet.dto.UserDto;
+import es.eoi.facenet.entities.Assistance;
 import es.eoi.facenet.entities.Event;
+import es.eoi.facenet.entities.User;
 import es.eoi.facenet.services.AssistanceService;
 import es.eoi.facenet.services.EventService;
 
@@ -45,7 +49,7 @@ public class EventsController {
 		return new ResponseEntity<>(eventDto, HttpStatus.OK);
 	}
 	
-//	@RequestMapping(method = RequestMethod.POST, value = "/{id}")
+//	@RequestMapping(method = RequestMethod.POST)
 //	public 
 	
 //	@RequestMapping(method = RequestMethod.POST, value = "/{id}/yesAssistance")
@@ -54,25 +58,43 @@ public class EventsController {
 //	@RequestMapping(method = RequestMethod.POST, value = "/{id}/notAssistance")
 //	public
 	
-//	@RequestMapping(method = RequestMethod.GET, value = "{id}/users/yesAssistance")
-//	public ResponseEntity<List<AssistanceDto>> findByUsersYesAssistance() {
-//		return null;
-//	}
-//	
-//	@RequestMapping(method = RequestMethod.GET, value = "{id}/users/notAssistance")
-//	public ResponseEntity<List<AssistanceDto>> findByUsersNotAssistance() {
-//		return null;
-//	}
-//	
-//	@RequestMapping(method = RequestMethod.GET, value = "user/{id}/yesAssistance")
-//	public ResponseEntity<List<EventDto>> findByEventYesAssistance() {
-//		return null;
-//	}
-//	
-//	@RequestMapping(method = RequestMethod.GET, value = "user/{id}/notAssistance")
-//	public ResponseEntity<List<EventDto>> findByEventNotAssistance() {
-//		return null;
-//	}
-
+	@RequestMapping(method = RequestMethod.GET, value = "{id}/users/yesAssistance")
+	public ResponseEntity<List<UserDto>> findByUsersYesAssistance() {
+		ModelMapper mapper = new ModelMapper();
+		List<UserDto> userDto;
+		List<User> user = assistanceservice.findByUsersYesAssistance();
+		java.lang.reflect.Type targetListType = new TypeToken<List<AssistanceDto>>() {}.getType();
+		userDto = mapper.map(user, targetListType);
+		return new ResponseEntity<>(userDto, HttpStatus.OK);
+	}
 	
+	@RequestMapping(method = RequestMethod.GET, value = "{id}/users/notAssistance")
+	public ResponseEntity<List<UserDto>> findByUsersNotAssistance() {
+		ModelMapper mapper = new ModelMapper();
+		List<UserDto> userDto;
+		List<User> user = assistanceservice.findByUsersNotAssistance();
+		java.lang.reflect.Type targetListType = new TypeToken<List<AssistanceDto>>() {}.getType();
+		userDto = mapper.map(user, targetListType);
+		return new ResponseEntity<>(userDto, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "user/{id}/yesAssistance")
+	public ResponseEntity<List<EventDto>> findByEventYesAssistance() {
+		ModelMapper mapper = new ModelMapper();
+		List<EventDto> eventDto;
+		List<Event> event = assistanceservice.findByEventYesAssistance();
+		java.lang.reflect.Type targetListType = new TypeToken<List<AssistanceDto>>() {}.getType();
+		eventDto = mapper.map(event, targetListType);
+		return new ResponseEntity<>(eventDto, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "user/{id}/notAssistance")
+	public ResponseEntity<List<EventDto>> findByEventNotAssistance() {
+		ModelMapper mapper = new ModelMapper();
+		List<EventDto> eventDto;
+		List<Event> event = assistanceservice.findByEventNotAssistance();
+		java.lang.reflect.Type targetListType = new TypeToken<List<AssistanceDto>>() {}.getType();
+		eventDto = mapper.map(event, targetListType);
+		return new ResponseEntity<>(eventDto, HttpStatus.OK);
+	}
 }
