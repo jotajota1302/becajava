f79ec6fe37affd3a3a2bf6080cd010476b8076e6
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-07-14 19:39:17 +0200

    commit

--- SpringDataJPA/src/main/java/edu/es/eoi/repository/UserRepository.java
+++ SpringDataJPA/src/main/java/edu/es/eoi/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package edu.es.eoi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import edu.es.eoi.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	public List<User> findByNombreOrSaldo(String nombre,Double saldo);
+	
 
 }
