a16ef575a7c19c8509a6a3b96338602e938e3171
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-07-14 20:51:38 +0200

    commit

--- SpringDataJPA/src/test/java/edu/es/eoi/UserRepositoryTest.java
+++ SpringDataJPA/src/test/java/edu/es/eoi/UserRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,7 +25,14 @@ public class UserRepositoryTest {
 		System.out.println("hola");
 		repository.findByNombreOrSaldo("JJ",50.0);
 		repository.findById(1);
+		repository.findFirstByOrderBySaldoDesc();
 		System.out.println("adios");
+		
+		//paginación, primeros 2 resultados
+		Pageable firstPageWithTwoElements = PageRequest.of(1, 4);
+		repository.findAll(firstPageWithTwoElements);
+		System.out.println("adios");
+		
 	}
 
 	
